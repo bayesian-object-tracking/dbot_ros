91e7adca37a5ab528883c3c8bc8d7f7154cb733d
issac <jan.issac@gmail.com>
2015-12-03 19:08:56 +0100

    Implemented Ros camera data provider

---
+++ include/dbot_ros_pkg/utils/ros_camera_data_provider.hpp
@@ -0,0 +1,83 @@
+/*
+ * This is part of the Bayesian Object Tracking (bot),
+ * (https://github.com/bayesian-object-tracking)
+ *
+ * Copyright (c) 2015 Max Planck Society,
+ * 				 Autonomous Motion Department,
+ * 			     Institute for Intelligent Systems
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public
+ * License License (GNU GPL). A copy of the license can be found in the LICENSE
+ * file distributed with this source code.
+ */
+
+/**
+ * \file ros_camera_data_provider.hpp
+ * \author Jan Issc (jan.issac@gmail.com)
+ * \date December 2015
+ */
+
+#pragma once
+
+#include <ros/ros.h>
+
+#include <string>
+
+#include <Eigen/Dense>
+
+#include <dbot/util/camera_data_provider.hpp>
+
+namespace dbot
+{
+class RosCameraDataProvider : public CameraDataProvider
+{
+public:
+    /**
+     * \brief Creates a RosCameraDataProvider
+     * \param nh
+     * 			ros::NodeHandle instance used to read topic data
+     * \param camera_info_topic
+     * 			Camera info topic name
+     * \param downsampling_factor
+     * 			Resolution downsampling factor
+     * \param timeout
+     * 			Timeout in seconds applied on topic requests
+     */
+    RosCameraDataProvider(const ros::NodeHandle& nh,
+                          const std::string& camera_info_topic,
+                          const std::string& depth_image_topic,
+                          int downsampling_factor,
+                          double timeout);
+
+public:
+    /**
+     * \brief returns an obtained depth image as an Eigen matrix from depth
+     *        image topic
+     */
+    Eigen::MatrixXd depth_image() const;
+
+    /**
+     * \brief Reads and returns the camera matrix from the camera info topic and
+     * adapts its enties if the downsampling_factor is not equal 1.
+     */
+    Eigen::Matrix3d camera_matrix() const;
+
+    /**
+     * \brief Reads and returns the current camera frame obtained from the
+     * camera info topic
+     */
+    std::string frame_id() const;
+
+    /**
+     * \brief Returns the downsampling_factor defined on construction
+     */
+    int downsampling_factor() const;
+
+private:
+    mutable ros::NodeHandle nh_;
+    std::string camera_info_topic_;
+    std::string depth_image_topic_;
+    int downsampling_factor_;
+    double timeout_;
+};
+}
---
+++ src/dbot_ros_pkg/utils/ros_camera_data_provider.cpp
@@ -0,0 +1,69 @@
+/*
+ * This is part of the Bayesian Object Tracking (bot),
+ * (https://github.com/bayesian-object-tracking)
+ *
+ * Copyright (c) 2015 Max Planck Society,
+ * 				 Autonomous Motion Department,
+ * 			     Institute for Intelligent Systems
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public
+ * License License (GNU GPL). A copy of the license can be found in the LICENSE
+ * file distributed with this source code.
+ */
+
+/**
+ * \file ros_camera_data_provider.hpp
+ * \author Jan Issc (jan.issac@gmail.com)
+ * \date December 2015
+ */
+
+#include <sensor_msgs/Image.h>
+
+#include <dbot_ros_pkg/utils/ros_interface.hpp>
+#include <dbot_ros_pkg/utils/ros_camera_data_provider.hpp>
+
+namespace dbot
+{
+RosCameraDataProvider::RosCameraDataProvider(
+    const ros::NodeHandle& nh,
+    const std::string& camera_info_topic,
+    const std::string& depth_image_topic,
+    int downsampling_factor,
+    double timeout)
+    : nh_(nh),
+      camera_info_topic_(camera_info_topic),
+      depth_image_topic_(depth_image_topic),
+      downsampling_factor_(downsampling_factor),
+      timeout_(timeout)
+{
+}
+
+Eigen::MatrixXd RosCameraDataProvider::depth_image() const
+{
+    sensor_msgs::Image::ConstPtr ros_image =
+        ros::topic::waitForMessage<sensor_msgs::Image>(
+            depth_image_topic_, nh_, ros::Duration(timeout_));
+
+    auto image = ri::Ros2Eigen<double>(*ros_image, downsampling_factor_);
+
+    return image;
+}
+
+Eigen::Matrix3d RosCameraDataProvider::camera_matrix() const
+{
+    auto cam_mat =
+        ri::GetCameraMatrix<double>(camera_info_topic_, nh_, timeout_);
+    cam_mat.topLeftCorner(2, 3) /= downsampling_factor();
+    return cam_mat;
+}
+
+std::string RosCameraDataProvider::frame_id() const
+{
+    return ri::GetCameraFrame<double>(camera_info_topic_, nh_, timeout_);
+}
+
+int RosCameraDataProvider::downsampling_factor() const
+{
+    return downsampling_factor_;
+}
+}
