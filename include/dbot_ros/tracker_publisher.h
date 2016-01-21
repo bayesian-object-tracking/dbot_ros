5dedfbae58f88283c4fe66ba7982d6bbd64c0488
issac <jan.issac@gmail.com>
2016-01-21 16:30:58 +0100

    Added Tracker publisher interface

---
+++ include/dbot_ros/tracker_publisher.h
@@ -0,0 +1,44 @@
+/*
+ * This is part of the Bayesian Object Tracking (bot),
+ * (https://github.com/bayesian-object-tracking)
+ *
+ * Copyright (c) 2015 Max Planck Society,
+ * 				 Autonomous Motion Department,
+ * 			     Institute for Intelligent Systems
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public
+ * License License (GNU GPL). A copy of the license can be found in the LICENSE
+ * file distributed with this source code.
+ */
+
+/**
+ * \file tracker_publisher.h
+ * \date January 2016
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#pragma once
+
+#include <sensor_msgs/Image.h>
+#include <dbot/util/camera_data.hpp>
+
+namespace dbot
+{
+/**
+ * \brief Represents the tracker publisher interface. A tracker publisher may
+ * publish the estimated state result along object markers, robot models,
+ * point clouds and images.
+ */
+template <typename Tracker>
+class TrackerPublisher
+{
+public:
+    typedef typename Tracker::State State;
+
+public:
+    virtual void publish(
+        const State& state,
+        const sensor_msgs::Image& image,
+        const std::shared_ptr<dbot::CameraData>& camera_data) = 0;
+};
+}
