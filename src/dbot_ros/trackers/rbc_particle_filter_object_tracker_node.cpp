62320fa2742bbdec5aa720080cec34ff6cd2b98c
issac <jan.issac@gmail.com>
2016-03-21 15:11:52 +0100

    updated to changes made in tracker node

--- src/dbot_ros/trackers/rbc_particle_filter_object_tracker_node.cpp
+++ src/dbot_ros/trackers/rbc_particle_filter_object_tracker_node.cpp
@@ -221,7 +221,7 @@ int main(int argc, char** argv)
     /* - Create tracker node        - */
     /* ------------------------------ */
 
-    dbot::TrackerNode<Tracker> tracker_node(tracker, tracker_publisher);
+    dbot::TrackerNode<Tracker> tracker_node(tracker, camera_data, tracker_publisher);
 
     /* ------------------------------ */
     /* - Initialize interactively   - */
